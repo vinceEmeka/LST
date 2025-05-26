45d7f011ade8b874d5dc3a11e1b613bbab2b687c
Vinz <abc@xyz.com>
2025-05-26 22:45:18 +0100

    pictures not deploying

--- src/sections/hero section/Hero.tsx
+++ src/sections/hero section/Hero.tsx
@@ -1,18 +1,24 @@
 
 import './Hero.css'
+
+import img1 from '../assets/img/media bg-cover (1).png';
+import img2 from '../assets/img/media bg-cover.png';
+import img3 from '../assets/img/media bg-cover (2).png';
+import img4 from '../assets/img/media bg-cover (3).png';
+
 export default function Hero() {
   return (
     <div className="hero">
       <div className="left">
-       <img className = 'img1' src="src\assets\img\media bg-cover (1).png" alt="" />
+       <img className = 'img1' src={img1} alt="" />
       </div>
       <div className="right">
        <div className="upper">
-        <img className = 'img2'src="src\assets\img\media bg-cover.png" alt="" />
+        <img className = 'img2'src={img2} alt="" />
        </div>
        <div className="lower">
-         <img className = 'img3' src="src\assets\img\media bg-cover (2).png" alt="" />
-          <img className = 'img4' style={{width: '100%'}} src="src\assets\img\media bg-cover (3).png" alt="" />
+         <img className = 'img3' src={img3} alt="" />
+          <img className = 'img4' style={{width: '100%'}} src={img4} alt="" />
          </div>
         </div>
        </div>
